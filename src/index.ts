1046c5b0d9c7222eb6df99582a804d20c91c5206
Yasuhiro Arakawa <arakawa@matsukura-sangyo.co.jp>
2025-08-01 10:39:56 +0900

    ・外部公開クラスの見直し

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ export {
     CotomyDebugSettings
 } from "./debug";
 export {
-    CotomyActionEvent, CotomyApiFailedEvent, CotomyApiForm, CotomyEntityFillApiForm as CotomyFillApiForm,
+    CotomyActionEvent, CotomyApiFailedEvent, CotomyApiForm, CotomyEntityApiForm, CotomyEntityFillApiForm,
     CotomyForm,
     CotomyQueryForm
 } from "./form";
